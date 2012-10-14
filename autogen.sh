7a4d5b8e89f191a5f8fd289415e0e2aa89fc0901
Sebastian Harl <sh@tokkee.org>
2012-10-14 22:41:09 +0200

    autogen.sh: Improved "help" output.

--- autogen.sh
+++ autogen.sh
@@ -15,6 +15,9 @@ autoconf --force --warnings=all
 autoheader --force --warnings=all
 automake --add-missing --copy --foreign --warnings=all
 
+set +x
+
+echo ""
 echo "Run './configure' to configure the software. See './configure --help'"
 echo "for details on command line options and environment variables affecting"
 echo "the build."
